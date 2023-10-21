5367f65e9065046655f521ee93159ea577e34960
Abenezer Mengistu <120439256+Abcreed21@users.noreply.github.com>
2023-10-21 15:05:20 +0300

    Update settings.py

--- Melhik/Melhik/settings.py
+++ Melhik/Melhik/settings.py
@@ -126,7 +126,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'abcreed21@gmail.com'  
-EMAIL_HOST_PASSWORD = 'anib gqcj vzbf dtlv'  
+EMAIL_HOST_PASSWORD = ''  
 EMAIL_USE_TLS = True 
 DEFAULT_FROM_EMAIL = 'abcreed21@gmail.com'
 
